8d2cdfcb1021a80994f10bdaedea1c360041ec9b
Lorenzo91347 <lorenzo.losito91@gmail.com>
2023-11-10 11:56:49 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+'use strict'
+//prompt per ogni numero
+
+const n1 = parseInt(prompt('inserisci un numero'));
+const n2 = parseInt(prompt('inserisci un numero'));
+const n3 = parseInt(prompt('inserisci un numero'));
+const n4 = parseInt(prompt('inserisci un numero'));
+const n5 = parseInt(prompt('inserisci un numero'));
+const n6 = parseInt(prompt('inserisci un numero'));
+const n7 = parseInt(prompt('inserisci un numero'));
+const n8 = parseInt(prompt('inserisci un numero'));
+const n9 = parseInt(prompt('inserisci un numero'));
+const n10 = parseInt(prompt('inserisci un numero'));
+
+//somma dei numeri
+
+const sum = n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + n10;
+
+//risultato finale
+
+console.log(sum);
